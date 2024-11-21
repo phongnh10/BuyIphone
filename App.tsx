83a9015ae82220346873b529aab5d8a6854bf03d
BanBan <pn93948848@gmail.com>
2024-11-21 15:18:03 +0700

    Update new

--- App.tsx
+++ App.tsx
@@ -2,10 +2,17 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
+  //test lan 1
   return (
     <View>
       <Text>React Native Agila</Text>
+<<<<<<< HEAD
       <Text>KHANG</Text>
+=======
+      <Text>Khang BIG GUN</Text>
+      
+
+>>>>>>> main
     </View>
   )
 }
